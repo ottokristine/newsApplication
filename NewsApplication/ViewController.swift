b08bb174b0b830964bead6a46a6649cb1436f222
Kristine Otto <kristineotto@kristines-macbook-pro.local>
2018-10-19 13:07:56 -0400

    Initial Commit

---
+++ NewsApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApplication
+//
+//  Created by Kristine Otto on 10/19/18.
+//  Copyright © 2018 Kristine Otto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NewsApplicationTests/NewsApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewsApplicationTests.swift
+//  NewsApplicationTests
+//
+//  Created by Kristine Otto on 10/19/18.
+//  Copyright © 2018 Kristine Otto. All rights reserved.
+//
+
+import XCTest
+@testable import NewsApplication
+
+class NewsApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
